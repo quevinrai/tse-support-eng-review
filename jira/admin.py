35e8219a17409568c2a75890b437e4b3bc78f137
Quevin Bambasi <quevin.bambasi@color.com>
2023-10-08 18:40:14 -0400

    Make migrations

--- jira/admin.py
+++ jira/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import MonthlyQuickNumber
 
 # Register your models here.
+admin.site.register(MonthlyQuickNumber)
--- jira/models.py
+++ jira/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class MonthlyQuickNumber(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=250, blank=True)
+    jql = models.TextField()
+    url_param_value = models.TextField(default='')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Monthly Quick Number"
+        verbose_name_plural = "Monthly Quick Numbers"
