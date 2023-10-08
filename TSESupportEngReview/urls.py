5d28342e7451c9deb452dc045b200d2ef2fc903a
Quevin Bambasi <quevin.bambasi@color.com>
2023-10-08 18:42:00 -0400

    Add path for '<month>/<year>'

--- TSESupportEngReview/urls.py
+++ TSESupportEngReview/urls.py
@@ -20,5 +20,6 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('<str:month>/<str:year>', views.month_year, name='month_year')
 ]
