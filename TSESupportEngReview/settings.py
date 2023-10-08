b8979ad0efa2e6084f04021ed8ced59c80fddaaf
Quevin Bambasi <quevin.bambasi@color.com>
2023-10-08 18:41:28 -0400

    Add comma

--- TSESupportEngReview/settings.py
+++ TSESupportEngReview/settings.py
@@ -38,9 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
     'jira',
-    'TSESupportEngReview'
+    'TSESupportEngReview',
 ]
 
 MIDDLEWARE = [
