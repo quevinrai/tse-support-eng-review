7cf1107fbce89b66943688962d08e30ade972f62
Quevin Bambasi <quevin.bambasi@color.com>
2023-10-08 18:42:54 -0400

    Add month_year function • Import re, render, and MonthlyQuickNumber

--- TSESupportEngReview/views.py
+++ TSESupportEngReview/views.py
@@ -1,4 +1,22 @@
+import re
+
 from django.shortcuts import render
+from jira.models import MonthlyQuickNumber
 
 def home(request):
+    data = MonthlyQuickNumber.objects.all()
+    print(data.values()[0]['url_param_value'])
+    
     return render(request, 'TSESupportEngReview/index.html')
+
+def month_year(request, month, year):
+    context = {
+        'month': month,
+        'year': year
+    }
+
+    data = MonthlyQuickNumber.objects.all()
+    created_gte = re.search('created\s>=\s(\d{4})-(\d{2})-(\d{2})', data.values()[0]['jql'])
+    print(created_gte.group(1) + '/' + created_gte.group(2) + '/' + created_gte.group(3))
+
+    return render(request, 'TSESupportEngReview/index.html', context)
