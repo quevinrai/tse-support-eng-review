c7b477deabbd054a7df142b10650604d2b82e86d
Quevin Bambasi <quevin.bambasi@color.com>
2023-10-21 13:25:07 -0400

    Add logic for getting previous and current month • Still testing • Incomplete code

--- TSESupportEngReview/views.py
+++ TSESupportEngReview/views.py
@@ -1,13 +1,37 @@
 import re
+import datetime
 
+from datetime import datetime, timedelta
 from django.shortcuts import render
 from jira.models import MonthlyQuickNumber
 
-def home(request):
-    data = MonthlyQuickNumber.objects.all()
-    print(data.values()[0]['url_param_value'])
+"""
+FUNCTION-BASED VIEWS
+"""
 
-    return render(request, 'TSESupportEngReview/index.html')
+def home(request):
+    current_month = datetime.now().replace(day=1)
+    current_month_full = datetime.now().strftime('%B')
+    current_month_num = datetime.now().strftime('%m')
+    current_year = datetime.now().strftime('%Y')
+
+    previous_month = (datetime.utcnow().replace(day=1) - timedelta(days=1)).replace(day=1)
+    print(f'Previous month: {previous_month.strftime("%Y")}-{previous_month.strftime("%m")}-{previous_month.strftime("%d")}')
+    # print(f'Previous month: {current_month.strftime("%Y")}-{current_month.strftime("%m")}-{current_month.strftime("%d")}')
+
+    new_created_date_gte = f'created >= {current_year}-{current_month_num}-01'
+    new_created_date_lte = f'created <= {current_year}-{current_month_num}-01'
+
+    data = MonthlyQuickNumber.objects.all()
+    created_gte = re.sub('created\s>=\s(\d{4})-(\d{2})-(\d{2})', new_created_date_gte, data.values()[0]['jql'])
+    # print(created_gte)
+
+    context = {
+        'month': current_month_full,
+        'year': current_year
+    }
+
+    return render(request, 'TSESupportEngReview/index.html', context)
 
 def month_year(request, month, year):
     context = {
@@ -20,3 +44,10 @@ def month_year(request, month, year):
     print(created_gte.group(1) + '/' + created_gte.group(2) + '/' + created_gte.group(3))
 
     return render(request, 'TSESupportEngReview/index.html', context)
+
+"""
+UTILITY FUNCTIONS
+"""
+
+def update_created_date(previous_month, current_month, data):
+    pass
